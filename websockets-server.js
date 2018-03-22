d3fc453688859cd138fd05e95e47aeffc56aee7a
elasanchez <luigi.easanchez@gmail.com>
2018-03-21 20:23:24 -0700

    implemented the ability for users to have a topic and change topics

--- websockets-server.js
+++ websockets-server.js
@@ -5,12 +5,18 @@ var ws = new WebSocketServer({
   port: port
 });
 var messages = [];
-
+var topic;
 
 console.log('websockets server started');
 
 ws.on('connection', function(socket) {
   console.log('client connection established');
+
+  if (topic) {
+    var curr = "*** Topic is ";
+    curr += "'" + topic + "'";
+    socket.send(curr);
+  }
 
   messages.forEach(function(msg) {
     socket.send(msg);
@@ -18,10 +24,26 @@ ws.on('connection', function(socket) {
 
   socket.on('message', function(data) {
     console.log('message received: ' + data);
+
+    if(data.indexOf("/topic") != -1) {
+      var newTopic = "*** Topic has been changed to ";
+      // get the data after topic_ till the end of the message
+      newTopic += "'" + data.substring(7, data.length) + "'";
+      //keep track of topic
+      topic = data.substring(7, data.length);
+
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(newTopic);
+      });
+    }
+
+
     messages.push(data);
     ws.clients.forEach(function(clientSocket) {
-    clientSocket.send(data)
-  });
+      clientSocket.send(data)
+    });
+
+    // previous implementation to send data to main chat
     // socket.send(data);
   });
 });
